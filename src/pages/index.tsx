7c01e34589d9c64733dea388f74c63096a6f762b
nhutnhm <nhutnhm@edoctor.vn>
2023-06-26 18:51:48 +0700

    i18

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,32 @@
+import { GetStaticProps } from "next";
+
 import Image from "next/image";
+
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { t } = useTranslation();
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+      <h1>{t("common:title")}</h1>
+      <h1>{t("home:title")}</h1>
       <div>2</div>
     </main>
   );
 }
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale as string, ["common", "home"])),
+    },
+  };
+};
