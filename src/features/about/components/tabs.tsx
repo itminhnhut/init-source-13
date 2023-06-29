1bde13350b2a29881330c34f0c7e66e1fda928c2
nhutnhm <nhutnhm@edoctor.vn>
2023-06-29 16:08:47 +0700

    about

--- src/features/about/components/tabs.tsx
+++ src/features/about/components/tabs.tsx
@@ -61,7 +61,7 @@ const TabsAbout = () => {
             <h1 className="mt-32 text-center text-[64px] font-semibold">About Us</h1>
             <section className="w-[806px]">
                 <Tabs defaultIndex={tabIndex}>
-                    <TabList className="mb-6 flex cursor-pointer flex-row justify-center gap-x-6 text-lg font-medium">
+                    <TabList className="mb-6 flex cursor-pointer flex-row justify-center  gap-x-6 text-lg font-medium">
                         {renderTab()}
                     </TabList>
 
@@ -73,14 +73,12 @@ const TabsAbout = () => {
                                 wider blockchain ecosystem. Binance Labs is committed to supporting fast-executing,
                                 technical teams who positively impact the crypto space and build the decentralised web.
                             </h2>
-                            <section className="mt-[30px] flex flex-row justify-center gap-x-10">
+                            <section className="mt-[30px] flex flex-row gap-x-10">
                                 {TAB_PANEL_1?.map((item: { title: string; subTitle: string }) => {
                                     return (
-                                        <section className="cursor-pointer" key={`Our_Story_${item.subTitle}`}>
-                                            <section className="text-gradient-1 text-[32px] font-medium">
-                                                {item.title}
-                                            </section>
-                                            <section className="font-medium">{item.subTitle}</section>
+                                        <section className="" key={`Our_Story_${item.subTitle}`}>
+                                            <section>{item.title}</section>
+                                            <section>{item.subTitle}</section>
                                         </section>
                                     )
                                 })}
@@ -88,48 +86,10 @@ const TabsAbout = () => {
                         </article>
                     </TabPanel>
                     <TabPanel>
-                        <article>
-                            <h2 className="w-full text-center font-light leading-6">
-                                Binance Labs identifies, invests, and empowers viable blockchain entrepreneurs,
-                                startups, and communities, providing financing to industry projects that help grow the
-                                wider blockchain ecosystem. Binance Labs is committed to supporting fast-executing,
-                                technical teams who positively impact the crypto space and build the decentralised web.
-                            </h2>
-                            <section className="mt-[30px] flex flex-row justify-center gap-x-10">
-                                {TAB_PANEL_1?.map((item: { title: string; subTitle: string }) => {
-                                    return (
-                                        <section className="cursor-pointer" key={`Our_Story_${item.subTitle}`}>
-                                            <section className="text-gradient-1 text-[32px] font-medium">
-                                                {item.title}
-                                            </section>
-                                            <section className="font-medium">{item.subTitle}</section>
-                                        </section>
-                                    )
-                                })}
-                            </section>
-                        </article>
+                        <h2>Mission</h2>
                     </TabPanel>
                     <TabPanel>
-                        <article>
-                            <h2 className="w-full text-center font-light leading-6">
-                                Binance Labs identifies, invests, and empowers viable blockchain entrepreneurs,
-                                startups, and communities, providing financing to industry projects that help grow the
-                                wider blockchain ecosystem. Binance Labs is committed to supporting fast-executing,
-                                technical teams who positively impact the crypto space and build the decentralised web.
-                            </h2>
-                            <section className="mt-[30px] flex flex-row justify-center gap-x-10">
-                                {TAB_PANEL_1?.map((item: { title: string; subTitle: string }) => {
-                                    return (
-                                        <section className="cursor-pointer" key={`Our_Story_${item.subTitle}`}>
-                                            <section className="text-gradient-1 text-[32px] font-medium">
-                                                {item.title}
-                                            </section>
-                                            <section className="font-medium">{item.subTitle}</section>
-                                        </section>
-                                    )
-                                })}
-                            </section>
-                        </article>
+                        <h2>Vision</h2>
                     </TabPanel>
                 </Tabs>
             </section>
