457b119080c68c30ae263530db9a384339777146
devNhutNami <nhutnhm@nami.trade>
2023-07-03 15:52:09 +0700

    mobi footer

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -3,11 +3,11 @@ const Footer = (): any => {
     <footer className="text-white flex flex-col justify-center items-center pb-6 absolute inset-x-0 bottom-0 opacity-50">
       <p className="text-sm font-light">Copyrights © 2023 SCI.com. All Rights Reserved.</p>
       <div className="flex flex-row text-sm font-normal">
-        <div>Terms of Use</div>
+        <div className="no-underline lg:underline underline-offset-2 cursor-pointer">Terms of Use</div>
         <ul className="list-outside list-disc ml-6">
           <li></li>
         </ul>
-        <div>Privacy Policy</div>
+        <div className="no-underline lg:underline underline-offset-2 cursor-pointer">Privacy Policy</div>
       </div>
     </footer>
   );
