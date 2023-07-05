b7d8644204b1cfdfadfa641c946755f89e09b5aa
devNhutNami <nhutnhm@nami.trade>
2023-07-05 17:55:35 +0700

    form

--- src/components/Elements/Button/index.tsx
+++ src/components/Elements/Button/index.tsx
@@ -7,9 +7,10 @@ interface LayoutI extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode
     className?: string
     iconType?: string
+    disabled?: boolean
 }
 
-const Button: FC<LayoutI> = ({ children, className, iconType, ...props }) => {
+const Button: FC<LayoutI> = ({ children, className, iconType, disabled = false, ...props }) => {
     const getIcon: any = {
         vector: '/images/button/ic_vector.svg',
         send: '/images/button/ic_send.svg',
@@ -19,6 +20,8 @@ const Button: FC<LayoutI> = ({ children, className, iconType, ...props }) => {
         <button
             className={classNames(className, 'flex cursor-pointer flex-row bg-gradient-2 px-6 py-4 font-medium rounded-[10px]')}
             {...props}
+            type={props.type || 'button'}
+            disabled
         >
             {children}
             {iconType && <Image className="ml-[10px]" width={24} height={24} src={getIcon?.[iconType]} alt="vector" />}
--- src/components/Form/InputField.tsx
+++ src/components/Form/InputField.tsx
@@ -1,44 +1,75 @@
+import { ChangeEvent, useMemo } from 'react'
+
+import { ErrorMessage } from "@hookform/error-message";
+
+
 import classNames from 'classnames'
+
 import { styled } from 'styled-components'
 
 type inputFieldProps = {
+    t?: any
+    name: string,
     className?: string
     label: string
     isRequired?: boolean
     placeholder?: string
+    value?: string | number
+    ref?: any
+    errors?: any
+    onChange: (data: any) => void
 }
 
-const inputField = ({ className, label, isRequired, placeholder }: inputFieldProps) => {
+
+const InputField = ({ className, label, name, isRequired, placeholder, value, onChange, ref, errors, t }: inputFieldProps) => {
+
+    const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault()
+        onChange(e.target.value)
+    }
+
+    const msgErrByName = () => {
+        return t(`validation:${errors?.[name]?.type}`, { label: label.toLocaleLowerCase() })
+    }
+
     return (
-        <div className={classNames(className)}>
-            <label
-                htmlFor="first_name"
-                className={classNames('mb-2 block text-sm font-medium text-white', {
-                    hidden: !label,
-                    "after:pl-1 after:text-white after:content-['*']": isRequired,
-                })}
-            >
-                {label}
-            </label>
-            {/* <StyledInputWrapper className="relative"> */}
-            <StyledInputWrapper
-                required
-                type="text"
-                id="first_name"
-                placeholder={placeholder}
-                className="relative h-12 w-full rounded-[10px] bg-black px-4 py-3 text-white opacity-50 outline-0"
-            />
-            {/* </StyledInputWrapper> */}
-        </div>
+        <>
+            <div className={classNames(className)}>
+                <label
+                    htmlFor={name}
+                    className={classNames('mb-2 block text-sm font-medium text-white', {
+                        hidden: !label,
+                        "after:pl-1 after:text-white after:content-['*']": isRequired,
+                    }, 'hover:bg-gradient-to-r')}
+                >
+                    {label}
+                </label>
+                <StyledInputWrapper
+                    ref={ref}
+                    type="text"
+                    value={value}
+                    id={name}
+                    placeholder={placeholder}
+                    onChange={(e: any) => handleOnChange(e)}
+                    className="relative h-12 w-full rounded-[10px] bg-black px-4 py-3 border-solid
+                 text-white opacity-50 outline-0 gradient-border-mask border-[1px] border-gray-1"
+                />
+            </div>
+            {errors?.[name] &&
+                <ErrorMessage
+                    errors={errors}
+                    name={name}
+                    render={() => <div className='text-sm font-light text-red-default'>{msgErrByName()}</div>}
+                />
+            }
+        </>
     )
 }
 
-inputField.defaultProps = {
+InputField.defaultProps = {
     placeholder: 'Enter',
 }
 
-const StyledInputWrapper = styled.input`
-   
-`
+const StyledInputWrapper = styled.input``
 
-export default inputField
+export default InputField
